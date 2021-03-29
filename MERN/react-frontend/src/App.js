0b4bc6fbc715a27356f365e73d0d4544149a1229
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-03-28 21:55:19 -0500

    Add frontend React app for making API requests

--- MERN/react-frontend/src/App.js
+++ MERN/react-frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import NavBar from "./components/NavBar.jsx";
+
+import SortDataByAge from "./pages/SortDataByAge";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/data/sort-by-age" exact component={SortDataByAge} />
+      </Switch>
+    </Router>
   );
 }
 
