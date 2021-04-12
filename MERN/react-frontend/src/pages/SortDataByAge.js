60babd6f6d34003db6ce238d9251487a16c2937d
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-04-11 23:47:11 -0500

    Change class from class component to functional component for cleaner code
    - Loop function contains the 5ht iteration of a method to speed up the react DOM rendering speed

--- MERN/react-frontend/src/pages/SortDataByAge.js
+++ MERN/react-frontend/src/pages/SortDataByAge.js
@@ -1,26 +1,90 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import apis from "../apis";
-
+/*
 class SortDataByAge extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
       columns: [],
-      isLoading: false,
+      isLoading: true,
     };
   }
 
   componentDidMount = async () => {
     this.setState({ isLoading: true });
     this.setState({ data: await apis.sortDataByAge() });
+    this.setState({ isLoading: false });
   };
 
   render() {
     const { data, isLoading } = this.state;
-    console.log(data);
-    return <div>Finished Sorting Data by Age</div>;
+    if (!isLoading) {
+      console.log(isLoading);
+      console.log(data.data);
+      // return <div>helloo</div>;
+      //return data.data[0]._id;
+      return data.data.map((element) => {
+        return (
+          <li>
+            ID: {element.ID}, Name: {element.FirstNameLastName}, Email:{" "}
+            {element.EmailAddress}
+          </li>
+        );
+      });
+      //return data.map((element) => <li>{element}</li>);
+    } else {
+      console.log(isLoading);
+      return <div>Loading Still</div>;
+    }
   }
-}
+}*/
 
-export default SortDataByAge;
+const SortDataByAge = (props) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  async function getData() {
+    try {
+      setLoading(true);
+      const data = await apis.sortDataByAge();
+      setData(data);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  if (error) return "Failed to load resource A";
+  let list;
+  if (!loading) {
+    list = data.data.map((element) => {
+      return (
+        <li key={element.ID}>
+          ID: {element.ID}, Name: {element.FirstNameLastName}, Email:{" "}
+          {element.EmailAddress}
+        </li>
+      );
+    });
+  }
+  const loop = () => {
+    let list = [];
+    for (var i = 0; i < data.data.length; i++) {
+      list.push(
+        "ID: " +
+          data.data[i].ID +
+          " Name: " +
+          data.data[i].FirstNameLastName +
+          " Email: " +
+          data.data[i].EmailAddress
+      );
+    }
+    return list;
+  };
+  return loading ? "Loading..." : loop();
+};
+
+export default React.memo(SortDataByAge);
